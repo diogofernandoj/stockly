039bfd23a6f5f88235c52f8e97dedfc8a8567a24
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-14 10:29:40 -0300

    feat: add Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./_components/sidebar";
 
 export const metadata: Metadata = {
   title: "Stockly",
@@ -18,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="h-full flex">
+          <Sidebar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
