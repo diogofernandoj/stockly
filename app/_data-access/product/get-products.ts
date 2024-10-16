0bb4cd9265a8bfb9ee7465b32522ac72200cd3c7
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-16 08:47:45 -0300

    feat: add getProducts data access

---
+++ app/_data-access/product/get-products.ts
@@ -0,0 +1,9 @@
+import { db } from "@/app/_lib/prisma";
+import { Product } from "@prisma/client";
+import { cache } from "react";
+
+export const getProducts = async (): Promise<Product[]> => {
+  return await db.product.findMany({});
+};
+
+export const cachedGetProducts = cache(getProducts);
