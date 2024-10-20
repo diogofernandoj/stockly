0b9b4c6ce60604c89e358c8367047b8c44fdac42
Diogo Jorge <diogofernandoj@gmail.com>
2024-10-20 11:56:24 -0300

    feat: ensure product stock is restored on deleting sale

--- app/_actions/sale/delete-sale/index.ts
+++ app/_actions/sale/delete-sale/index.ts
@@ -7,9 +7,27 @@ import { revalidatePath } from "next/cache";
 export const deleteSale = async (data: DeleteSaleSchema) => {
   deleteSaleSchema.parse(data);
 
-  await db.sale.delete({
-    where: data,
-  });
+  await db.$transaction(async (trx) => {
+    const sale = await trx.sale.findUnique({
+      where: { id: data.id },
+      include: { saleProducts: true },
+    });
+    if (!sale) return;
 
+    for (const saleProduct of sale.saleProducts) {
+      await trx.product.update({
+        where: { id: saleProduct.productId },
+        data: {
+          stock: { increment: saleProduct.quantity },
+        },
+      });
+    }
+
+    await trx.sale.delete({
+      where: data,
+    });
+  });
+  revalidatePath("/");
+  revalidatePath("/products");
   revalidatePath("/sales");
 };
